9b1c11dc780e8eff54466be801f8c5982cc55227
Daniel Porteous <daniel@dport.me>
2023-10-24 15:23:30 +0100

    Update Rust example

---
+++ rust/README.md
@@ -0,0 +1,11 @@
+# Rust
+
+Install deps like this:
+```
+cargo build
+```
+
+Run like this:
+```
+cargo run -- --node-api-url https://api.mainnet.aptoslabs.com --api-key <key>
+```
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,24 @@
 use anyhow::Result;
 use aptos_sdk::rest_client::{AptosBaseUrl, ClientBuilder};
 use aptos_sdk::types::account_address::AccountAddress;
+use clap::Parser;
 use url::Url;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(long)]
+    node_api_url: Url,
+
+    #[clap(long, env)]
+    api_key: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let url = Url::parse("https://api.mainnet.aptoslabs.com").unwrap();
-    let url = AptosBaseUrl::Custom(url);
-    let rest_client = ClientBuilder::new(url).api_key("<key>")?.build();
-    let account = rest_client
+    let args = Args::parse();
+    let url = AptosBaseUrl::Custom(args.node_api_url);
+    let node_api_client = ClientBuilder::new(url).api_key(&args.api_key)?.build();
+    let account = node_api_client
         .get_account(AccountAddress::from_str_strict("0x1")?)
         .await?;
     println!("Account: {:#?}", account);
