70a75838056dccd8b9dc6b9cba56691b0d5146da
Daniel Porteous <daniel@dport.me>
2023-10-24 15:13:48 +0100

    Add Rust example

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,16 @@
-use aptos_sdk::rest_client::Client;
+use anyhow::Result;
+use aptos_sdk::rest_client::{AptosBaseUrl, ClientBuilder};
+use aptos_sdk::types::account_address::AccountAddress;
+use url::Url;
 
-fn main() {
-    let rest_client = Client::new(NODE_URL.clone());
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = Url::parse("https://api.mainnet.aptoslabs.com").unwrap();
+    let url = AptosBaseUrl::Custom(url);
+    let rest_client = ClientBuilder::new(url).api_key("<key>")?.build();
+    let account = rest_client
+        .get_account(AccountAddress::from_str_strict("0x1")?)
+        .await?;
+    println!("Account: {:#?}", account);
+    Ok(())
 }
